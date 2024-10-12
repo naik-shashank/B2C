09b0812450b54ff52d62957e4a4dee27912c0e01
shashank_naik <shashanknaik39037@gmail.com>
2024-10-12 17:33:51 +0530

    Add files via upload

---
+++ routes/orderRoutes.js
@@ -0,0 +1,9 @@
+import express from "express"
+import authenicateUser from "../middleware/authHandler.js"
+import { newOrder,getAllOrders } from "../controllers/orderController.js"
+const router = express.Router()
+
+router.route("/order").post( newOrder) //authenicateUser,
+router.route("/order").get(getAllOrders);
+
+export default router
